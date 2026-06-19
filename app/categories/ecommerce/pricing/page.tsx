116433418035561b7904d1e5b1581afa78ced792
jettybay <ojetb1@yahoo.com>
2026-06-19 23:04:25 +0100

    nav homepage

--- app/categories/ecommerce/pricing/page.tsx
+++ app/categories/ecommerce/pricing/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import PricingComingSoonPage from "../../pricing-coming-soon";
+import PricingComingSoonPage from "@/app/categories/pricing-coming-soon";
 
 export default function EcommercePricingPage() {
   return <PricingComingSoonPage category="ecommerce" />;
--- app/categories/fintech/pricing/page.tsx
+++ app/categories/fintech/pricing/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import PricingComingSoonPage from "../../pricing-coming-soon";
+import PricingComingSoonPage from "@/app/categories/pricing-coming-soon";
 
 export default function FintechPricingPage() {
   return <PricingComingSoonPage category="fintech" />;
